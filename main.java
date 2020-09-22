9c226542317f2ba3718e5ca43d11a50fecacb714
Tamara WIMMER <tawimmer18@htl-grieskirchen.local>
2020-09-22 11:51:18 +0200

    Heyy

--- main.java
+++ main.java
@@ -2,6 +2,5 @@ public static void main(String[] args){
 	System.out.println("AAAAAAAAAAAAAAAAAAAHELLO");
 }
 Tamara Wimmer
-=======
-Simon Feichtlbauer all Rights reserved
->>>>>>> 37aa0247b23db0d069b9698d3951e97c73b79480
+
+Simon Feichtlbauer 
